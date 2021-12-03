dd7c3ae91e577428f3861ae37720ccd9a713a481
Emereole Chinduji <credo05@hotmail.fr>
2021-12-03 22:55:00 +0100

    Dark mode
    I have added a dark mode to my website for a better user experience.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
+const navLinks = document.querySelectorAll('.nav__link');
+let icon = document.getElementById("icon");
+let localData = localStorage.getItem("theme");
+
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
@@ -11,3 +14,31 @@ navLinks.forEach(link => {
         
     })
 })
+
+
+if(localStorage.getItem("theme") === null)
+{
+    localStorage.setItem("theme", "light");
+}
+
+if(localData === "light")
+{
+    icon.src = "./images/moon.png";
+    document.body.classList.remove("dark-theme");
+}
+else if(localData === "dark")
+{
+    icon.src = "./images/sun.png";
+    document.body.classList.add("dark-theme");
+}
+
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        icon.src = "./images/sun.png";
+        localStorage.setItem("theme", "dark");
+    }else{
+        icon.src = "./images/moon.png";
+        localStorage.setItem("theme", "light");
+    }
+}
